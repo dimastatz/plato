81573f4ec5f63da2dd5eae8370cef3b65b4d7c59
dimastatz <dima.statz@gmail.com>
2023-06-18 21:02:32 +0300

    add plato

--- run_build_docker.sh
+++ run_build_docker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+black .
 docker build --tag plato-docker --build-arg CACHEBUST=$(date +%s) .
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,9 +3,9 @@ import plato.plato_facade as pf
 
 
 def test_upload_workbook():
-    body = b''
+    body = b""
     assert pf.upload_workbook(body) == 0
 
 
 def test_send_message():
-    assert pf.send_message('') == ''
+    assert pf.send_message("") == ""
