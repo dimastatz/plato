beb7b1b890fafb0d918a22f4679b184c417e1a10
dimastatz <dima.statz@gmail.com>
2023-06-20 22:01:30 +0300

    add static content

--- plato/app.py
+++ plato/app.py
@@ -4,27 +4,13 @@ import os
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route("/")
 def hello_world():
     """default flask handler"""
-    return """
-    <div align="center">
-        <h1 align="center"> Plato </h1> 
-        <h3>Unlock the Power of Problem-Solving!</br></h3>
-        <h4 align="center">
-            Empower young minds with our cutting-edge generative AI service, offering step-by-step solutions to math and logic puzzles. From basic arithmetic to complex problem-solving, Plato provides kids with personalized guidance, sparking curiosity and fostering a love for learning. Watch them excel, one solution at a time! 
-        </h4>
-        <img src="https://img.shields.io/badge/Progress-1%25-red"> <img src="https://img.shields.io/badge/Feedback-Welcome-green">
-        </br>
-        </br>
-        <kbd>
-        <img src="../docs/images/plato_1.png"> 
-        </kbd>
-    </div>
-    """
+    return app.send_static_file('index.html')
 
 
 if __name__ == "__main__":
