f0d2dc70661616d74fed3be661aea0deeefb6d64
dimastatz <dima.statz@gmail.com>
2023-06-20 09:57:36 +0300

    rebuild containers

--- run_build.sh
+++ run_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# stop and remove all conatiners
-docker ps -aq | xargs docker rm -f
-docker rmi $(docker images -q)
+# stop and remove smarti
+docker stop smarti-container
+docker rm smarti-container
+docker rmi smarti-image
 
 # test
 black plato
@@ -10,5 +11,5 @@ pylint plato
 pytest plato
 
 
-docker build --tag plato-docker --build-arg CACHEBUST=$(date +%s) .
-docker run -p 8888:8888 -d plato-docker
+docker build --tag smarti-image --build-arg CACHEBUST=$(date +%s) .
+docker run --name smarti-container -p 8888:8888 -d smarti-image
