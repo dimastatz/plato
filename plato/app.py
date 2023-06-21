e139c9d37313af6e36108245ac9d4e8f16f513f6
dimastatz <dima.statz@gmail.com>
2023-06-21 09:45:53 +0300

    add logger

--- plato/app.py
+++ plato/app.py
@@ -1,16 +1,22 @@
 """flask app runner"""
 
 import os
+import sys
+import logging
 from flask import Flask
 
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path="/static")
+log = logging.getLogger("app")
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.StreamHandler(sys.stdout))
 
 
 @app.route("/")
 def hello_world():
     """default flask handler"""
-    return app.send_static_file('index.html')
+    log.info("hello_world invoked")
+    return app.send_static_file("index.html")
 
 
 if __name__ == "__main__":
